b667ca1d8afa4cde52ce881f6f25af84325122f6
Leonardo Willians Couto <willianscouto@hotmail.com>
2023-02-16 00:07:27 -0300

    Teste b3

--- readme.mk
+++ readme.mk
@@ -1,12 +1,45 @@
 Teste de Leonardo Willians Couto
 
-##Componentes para execução do Teste
+## Componentes para execução do Teste
 
-Instalação
 Visual Studio 2022
 Visual Code
 
 
+# Para execução do teste
+Para execução dos teste deve seguir o Seguimnte Procedimento
+
+## Open APi Visual Studio 2022
+No visual Studio 2022  abrir o projeto B3Teste.sln
+
+Executar a API  -  B3Teste.API
 
 
-##Componentes para execução do Teste
+## Open Web APp Visual Studio Code
+
+No visual studio code:
+
+Abrir a pasta   \01 - WebApp\b3-teste-webapp
+
+Utilize os comandos para rodar a APP
+
+### Instalar Angular
+
+
+npm install -g @angular/cli
+
+
+### Update Version angular
+
+npm Update
+
+Run WebApp
+
+ng serve -o
+
+
+## No botão Simular CDB 
+Informar o valor do Investimento no campo valor Monetario
+Informar a quantidade de meses para resgate 
+
+Ira mostrar como resultado uma projeção de Investimento
